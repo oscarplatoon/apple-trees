f54f272536424dfda51805ba3191e383123069d3
jung341416 <jung341416@gmail.com>
2021-06-03 17:29:09 -0400

    James White Jung Park Apple Tree

--- apple_tree.py
+++ apple_tree.py
@@ -1,16 +1,32 @@
+from apple import Apple
 class AppleTree:
+    apples = []
+    appleTreeAgeToHaveApple = 3
+    appleTreeLifeSpan = 100
     def __init__(self):
-        pass
-  
+        self.age = 0
+        self.height = 0
+        self.alive = True
     def age_tree(self):
-        pass
-   
+        self.age += 1
+        self.height += 1
+        for x in range(self.age, self.appleTreeLifeSpan):
+            self.apples.append(Apple(10))           
+        print(self.apples)
     def is_dead(self):
-        pass
-    
+        if self.age > self.appleTreeLifeSpan:
+            self.alive = True
+        else:
+            self.alive = False
+        return self.alive        
+
     def any_apples(self):
-        pass
+        if len(self.apples) > -1:
+            return True
+        else:
+            return False
 
     def pick_an_apple(self):
         raise Exception('No apples on your tree')
+    
         # Read the tests before coding.
