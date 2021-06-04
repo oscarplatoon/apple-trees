89f9c581bdc2315bc647332a68cbbc5e8e2b00e4
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-03 20:28:28 -0400

    Rel 2, apple picking and Apple_Tree Spec tests successful

--- apple_tree.py
+++ apple_tree.py
@@ -1,16 +1,32 @@
+from apple import Apple
+
 class AppleTree:
     def __init__(self):
-        pass
-  
+        self.age = 0
+        self.height = 0
+        self.apple_count = []
+
     def age_tree(self):
-        pass
+        self.age += 1
+        if self.height <= 300:
+            self.height += 10
+        if self.age >= 4:
+            new_apple = Apple(self.age)
+            self.apple_count.append(new_apple)
    
     def is_dead(self):
-        pass
+        if self.age < 100:
+            return False
+        return True
     
     def any_apples(self):
-        pass
+        if len(self.apple_count) > 0:
+            return True
+        else:
+            return False
 
     def pick_an_apple(self):
+        if self.any_apples():
+            return self.apple_count.pop()
         raise Exception('No apples on your tree')
-        # Read the tests before coding.
+
